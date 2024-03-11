aa27f15694d95eb94548e96a0df34dedcc36d99b
deibissilgado <davidsilgado@gmail.com>
2024-03-11 18:23:03 -0500

    ok

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -5,6 +5,21 @@ use Illuminate\Http\Request;
 
 class ApiFilter{
     protected $safeParams =[]; //parametro para filtrar modelos
-    protected $columnMap =[];  //mapear columnas
+    protected $columnMap =[];  //mapear columnas 
     protected $operatorMap =[];  //crea mapeo de operadores
+
+    //transformando la Query recibida
+    public function transform(Request $request){
+        $eloQuery =[]; //vacia
+        
+        foreach ($this->safeParams as $parm => $operators) {
+            $query = $request->query($parm);
+
+            if (!isset($query)) {
+                continue;
+            } 
+            $column = $this->columnMap[$parm];
+            
+        }
+    }
 }
