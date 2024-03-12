fe3ad88939be220638dfeccedc9dd8d9fa28c4ed
deibissilgado <davidsilgado@gmail.com>
2024-03-12 08:52:07 -0500

    se crea el filtro ClienteFilter

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -3,23 +3,38 @@
 namespace App\Filters;
 use Illuminate\Http\Request;
 
-class ApiFilter{
+class ApiFilter {
+
     protected $safeParams =[]; //parametro para filtrar modelos
     protected $columnMap =[];  //mapear columnas 
     protected $operatorMap =[];  //crea mapeo de operadores
 
     //transformando la Query recibida
     public function transform(Request $request){
+
         $eloQuery =[]; //vacia
         
         foreach ($this->safeParams as $parm => $operators) {
+
             $query = $request->query($parm);
 
             if (!isset($query)) {
                 continue;
             } 
-            $column = $this->columnMap[$parm];
-            
+
+            $column = $this->columnMap[$parm] ?? $parm;
+
+            foreach ($operators as  $operator) {
+
+               if (isset($query[$operator])) {
+                $eloQuery[]=[$column,$this->operatorMap[$operator],$query[$operator]];
+               }
+               
+            }           
+           
         }
+
+        return $eloQuery;
     }
+
 }
