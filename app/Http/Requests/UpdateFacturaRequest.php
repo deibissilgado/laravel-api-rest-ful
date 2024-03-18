f66a31fb16e303ebc1f6dbdb2ee1534eefc85cb8
deibissilgado <davidsilgado@gmail.com>
2024-03-18 10:22:41 -0500

    UpdateFacturaRequest cambios e n lina49

--- app/Http/Requests/UpdateFacturaRequest.php
+++ app/Http/Requests/UpdateFacturaRequest.php
@@ -33,13 +33,30 @@ class UpdateFacturaRequest extends FormRequest
               ];
             
         }else{
-            //metodo PATCH , no requiere todos los parametros
+            /*metodo PATCH , no requiere todos los parametros 
+            sometimes india que pudiera o no estar presente*/
             return [
-                'cantidad'=> 'required|numeric' ,
-                'estado'=> 'required|in:p,P,v,V,f,F',
-                'fechaFacturada' => 'required|date_format:Y-m-d H:i:s',
-                'fechaPagada' => 'date_format:Y-m-d H:i:s|nullable',
+                'cantidad'=> 'sometimes|required|numeric' ,
+                'estado'=> 'sometimes|required|in:p,P,v,V,f,F',
+                'fechaFacturada' => 'sometimes|required|date_format:Y-m-d H:i:s',
+                'fechaPagada' => 'sometimes|date_format:Y-m-d H:i:s|nullable',
             ];
         }
     }
+          /*El método prepareForValidation es un método utilizado en las clases de 
+    solicitudes (Request) en Laravel. Este método te permite manipular los 
+    datos de la solicitud antes de que se realice la validación de los mismos. */
+    protected function prepareForValidation(){
+      /* Los campos fechaFacturada  fechaPagada no están definidos 
+      de este modo en la base de datos */
+        if ($this->fechaFacturada || $this->fechaPagada) {
+            $this->merge([
+                'fecha_facturada' => $this->fechaFacturada,
+                'fecha_pagada' => $this->fechaPagada,
+            ]);
+        }
+  
+    }
+
+
 }
